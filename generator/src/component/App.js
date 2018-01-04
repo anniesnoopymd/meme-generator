70888220cf7a9577f24d4d20ea5b6c0c06ae4de4
anniesnoopymd <anniesnoopymd@gmail.com>
2018-01-04 20:12:29 +0800

    render images

--- generator/src/component/App.js
+++ generator/src/component/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../styles/index.css';
+
+import MemeItem from './MemeItem';
 
 class App extends Component {
+  constructor(){
+      super();
+
+      this.state = {
+        memeLimit: 10
+      }
+  }
+
   render(){
     return(
       <div>
         <h2> Welcome to the Meme Generator!</h2>
         {
-          this.props.memes.map((meme,index) => {
+          this.props.memes.slice(0, this.state.memeLimit).map((meme,index) => {
             return (
-              <h4 key={index}>{meme.name}</h4>
+              <MemeItem key={index} meme={meme} />
             )
           })
         }
+        <div className="meme-button" onClick={() => {
+          this.setState({memeLimit: this.state.memeLimit+10})
+        }}>Load 10 more memes...</div>
       </div>
     )
   }
