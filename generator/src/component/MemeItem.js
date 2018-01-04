88e1bbf68d003fd439a5dfb5838b33cfe3cd917e
anniesnoopymd <anniesnoopymd@gmail.com>
2018-01-04 21:35:57 +0800

    animating the memes

--- generator/src/component/MemeItem.js
+++ generator/src/component/MemeItem.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 
 
 class MemeItem extends Component{
+  constructor(){
+      super();
+
+      this.state = {
+        hovered: false
+      }
+  }
+
     render(){
       return (
-        <div>
+        <div
+          className="meme-item"
+          onMouseEnter={() => this.setState({ hovered: true })}
+          onMouseLeave={() => this.setState({ hovered: false })}
+        >
           <img
           src={this.props.meme.url}
           alt={this.props.meme.name}
-          />
-          <p>
+          className="meme-img"/>
+          <p className="meme-txt">
             {this.props.meme.name}
           </p>
         </div>
